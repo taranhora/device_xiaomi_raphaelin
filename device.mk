49354daae860354e5e8d9483d431580c741b0f2c
Bruno Martins <bgcngm@gmail.com>
2019-01-23 10:25:44 +0000

    raphael: Implement SunlightEnhancement LiveDisplay HAL
    Change-Id: I17b0215ad78d86989c146291552c5e6e7b6a689d
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -274,6 +274,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_raphael
 
+# Livedisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.0-service.raphael
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
