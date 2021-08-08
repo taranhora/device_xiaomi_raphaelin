8ba0c3c91a103f760e6e8fad289b2d85fff88503
wadiehabib <wadie4jesus@gmail.com>
2021-08-08 09:13:46 +0000

    raphael: nuke some prebuilt packages

---
+++ remove_packages/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := AppDirectedSMSService ConnMO DCMO MyVerizonServices OBDM_Permissions OemDmTrigger SprintDM SprintHM USCCDM VZWAPNLib VzwOmaTrigger YouTubeMusicPrebuilt obdm_stub ViaBrowser Eleven
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
