d829240d1296e8e03916aa4962fec28a8bcadb17
Taran107 <hora.cooltaran@gmail.com>
2021-09-15 15:35:50 +0530

    Blissify

--- arrow_raphael.mk
+++ arrow_raphael.mk
@@ -12,17 +12,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_raphael
+PRODUCT_NAME := bliss_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi K20 Pro
 PRODUCT_MANUFACTURER := Xiaomi
-
-DEVICE_MAINTAINER := TH779, kubersharma001
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
@@ -31,3 +29,14 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
 
 TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# FOD animations
+TARGET_WANTS_FOD_ANIMATIONS := true
+
+# Gapps
+IS_PHONE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+BLISS_BUILDTYPE := OFFICIAL
