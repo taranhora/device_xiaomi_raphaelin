aljbandeljevic/device_xiaomi_raphael

raphael: releasetools: Print a better message while flashing
 * we're never releasetools to flash firmware in a unified
   device tree, so let's print not scare the user by printing
   "patching firmware"

Signed-off-by: Joey Huab <joey@evolution-x.org>

releasetools.py
@@ -40,7 +40,7 @@ def AddImage(info, basename, dest):
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
-  info.script.Print("Patching firmware images...")
+  info.script.Print("Patching dtbo and vbmeta images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   return

taranhora/device_xiaomi_raphaelin

Change-Id: If7c4c7bccadc01e4c6b67b452a5c1efabc4ad919

releasetools.py
@@ -30,7 +30,7 @@ def AddImage(info, basename, dest):
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
 
 def OTA_InstallEnd(info):
-  info.script.Print("Patching firmware images...")
+  info.script.Print("Patching vbmeta and dtbo images...")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   return