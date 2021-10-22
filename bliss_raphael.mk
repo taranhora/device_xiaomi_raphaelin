aa8c2bef3881269feda3d6579e4bc6ee6ee3e651
Taran107 <hora.cooltaran@gmail.com>
2021-10-22 10:12:12 +0530

    raphael:blissify

--- bliss_raphael.mk
+++ bliss_raphael.mk
@@ -12,17 +12,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common bliss stuff.
+$(call inherit-product, vendor/bliss/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_raphael
+PRODUCT_NAME := bliss_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi K20 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-DEVICE_MAINTAINER := TH779, kubersharma001
 
 PRODUCT_CHARACTERISTICS := nosdcard
 
@@ -33,4 +32,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
 
-TARGET_INCLUDE_PIXEL_CHARGER := true
+BLISS_BUILD_VARIANT := vanilla
