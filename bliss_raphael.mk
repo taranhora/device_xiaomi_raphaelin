4abd4caf81b52b7a9354f3d5c8b79c98695689d0
Taran107 <hora.cooltaran@gmail.com>
2022-01-13 00:36:32 +0530

    Blissify

--- bliss_raphael.mk
+++ bliss_raphael.mk
@@ -15,16 +15,16 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := spark_raphael
+PRODUCT_NAME := bliss_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := MI 9T Pro
+PRODUCT_MODEL := Redmi K20 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -32,5 +32,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# SparkOS stuff
-SPARK_BUILD_TYPE := Official
+BLISS_BUILD_VARIANT := gapps
+BLISS_BUILDTYPE := OFFICIAL
